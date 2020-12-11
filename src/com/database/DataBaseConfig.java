861c13b89830d7b9a10c40872ec5709ef0ec0c06
Sunil D M <sunildm4u@gmail.com>
2020-12-11 17:42:25 +0530

    Update DataBaseConfig.java

--- src/com/database/DataBaseConfig.java
+++ src/com/database/DataBaseConfig.java
@@ -4,6 +4,6 @@ public interface DataBaseConfig {
 	public static final String DRIVER="com.mysql.jdbc.Driver";
 	public static final String ConnectionURL="jdbc:mysql://localhost:3306/athomeservices";
 	public static final String USERNAME="root";
-	public static final String PASSWORD="Facet@1431";
+	public static final String PASSWORD="<use a strong password >";
 
 }
